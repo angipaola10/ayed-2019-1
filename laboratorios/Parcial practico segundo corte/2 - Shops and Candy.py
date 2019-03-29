4fb3e8a282246d1256e0d52213cd7b52128672a4
unknown <2161454@labinfo.is.escuelaing.edu.co>
2019-03-29 14:42:27 -0500

    correccion

--- laboratorios/Parcial practico segundo corte/2 - Shops and Candy.py
+++ laboratorios/Parcial practico segundo corte/2 - Shops and Candy.py
@@ -5,11 +5,11 @@ def solve(n,x,c):
     for i in range(1,n+1):
         li[i] = li[i-1] + c[i-1]
         if i>2:
-            li[i] = min(dp[i], li[i-2]+ x +c[i-1])
+            li[i] = min(li[i], li[i-2]+ x +c[i-1])
         if i>3:
-            li[i] = min(dp[i], li[i-3]+ 2*x +c[i-1])
+            li[i] = min(li[i], li[i-3]+ 2*x +c[i-1])
         if i>4:
-            li[i] = min(dp[i], li[i-4]+ 3*x +c[i-1])
+            li[i] = min(li[i], li[i-4]+ 3*x +c[i-1])
             
     return li[n]
 
